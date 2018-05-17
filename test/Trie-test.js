79fd021b6bab9a575fc2d1f2a770cfb2f546f96d
Marika Ross <marika.ross@gmail.com>
2018-05-16 20:18:21 -0600

    Make tests for suggest method

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -9,6 +9,10 @@ describe('Trie', () => {
     trie = new Trie();
   });
 
+    it('should instantiate a new trie', () => {
+
+    })
+
     it('should have a root node', () => {
     expect(trie.root).to.deep.equal(new Node());
     });
@@ -20,11 +24,48 @@ describe('Trie', () => {
         trie.insert('dogs');
         trie.insert('doggie');
 
-      console.log(JSON.stringify(trie, null, 4))
-      console.log(trie.root.children)
       expect(Object.keys(trie.root.children)[0]).to.equal('d');
       });
 
-  it('should ')
+  it('should insert a new word', () => {
+    //setup
+
+    //execution
+    
+    //expect
   })
+
+  it('should insert multiple words', () => {
+
+  })
+
+  describe('suggest', () => {
+    it('should return an array', () => {
+      
+      let expectedArray = ['dog', 'double', 'door'];
+      
+      trie.insert('dog');
+      trie.insert('double');
+      trie.insert('door');
+      let actualArray = trie.suggest('d');
+      
+      console.log(JSON.stringify(trie, null, 4))
+      expect(expectedArray).to.deep.equal(actualArray);
+    })
+
+    it('should return an array that only includes words related to prefix', () => {
+      let expectedArray = ['dog', 'double', 'door'];
+
+      trie.insert('dog');
+      trie.insert('double');
+      trie.insert('door');
+      trie.insert('carrot');
+      let actualArray = trie.suggest('d');
+
+      expect(expectedArray).to.deep.equal(actualArray);
+    })
+
+  })
+
+
 })
